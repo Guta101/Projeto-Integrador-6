2a3886794516e27cfb93fa1fb3d71c73ffd4d60a
Guta101 <gustavo.tartaglia2@gmail.com>
2022-09-14 17:58:34 -0300

    Placeholder Sprites Added

--- Assets/Scripts/Player/ScriptableObjects/PlayerParts/PlayerEquippablePart.cs
+++ Assets/Scripts/Player/ScriptableObjects/PlayerParts/PlayerEquippablePart.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Player/EquippablePart")]
 public class PlayerEquippablePart : ScriptableObject
 {
+    //  Sprite
+    [SerializeField] private Sprite partIcon;
+    public Sprite PartIcon { get { return partIcon; } private set { partIcon = value; } }
+
     //  Item Equipped
     public EquipableInterface equippedItem;
 
--- Assets/Scripts/UI/Inventory/EquipmentSlot.cs
+++ Assets/Scripts/UI/Inventory/EquipmentSlot.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EquipmentSlot : MonoBehaviour
 {
     private PlayerEquippablePart part;
+    [SerializeField] private Image partIcon;
+    [SerializeField] private Image itemIcon;
 
     public void Init(PlayerEquippablePart part)
     {
         this.part = part;
+        partIcon.sprite = part.PartIcon;
     }
 }
